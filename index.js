2f5f5cd7a7ac7ed2df4ee3d0543a740b1f39189b
Ignacio <waiken2k@hotmail.com>
2017-03-14 22:04:29 +0100

    V00.00.01 First promising apechusque start

---
+++ index.js
@@ -0,0 +1,33 @@
+var x = 2
+var y = 5
+
+console.log(x + y)
+
+function suma(x, y) {
+    return (x + y);
+}
+
+console.log(suma(x, y));
+
+/*  Uso de librerias
+ se puedne importar en cualquier parte del codigo pero mejor al principio */
+
+/* nueva variable http va a recibir el import que le hagamos con el require
+* esto es el equivalente de un import */
+var http = require('http');
+
+/* executa el create server que se pone a la escucha */
+var server = http.createServer(function (request, response) {
+    // Definicion de la funcion como los events
+    response.end("Apechusqueitor " + request.url)
+})
+
+
+// server.listen(8080, suma(x,y));
+
+server.listen(8080, function () {
+    console.log(" El servidor tiene una miaja de apechusque")
+});
+
+
+
